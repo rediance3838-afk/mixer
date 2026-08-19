93e0cce36b925715efc442f723a8331c1f1d4286
rediance3838-afk <rediance3838@gmail.com>
2026-08-19 16:38:52 +0530

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'dd3-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './manifest.webmanifest']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
